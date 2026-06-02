d82bbc31ed61ed44eda4c607a5b5e0bbac728158
liam <liam.edwards43@ed.cna.nl.ca>
2026-06-01 21:54:10 -0230

    Setting up the basics of the about page before adding any tailwind styling

---
+++ super-wacky-pc-building-site/app/about/page.js
@@ -0,0 +1,21 @@
+export default function AboutPage(){
+    return (
+        <div>
+            <h1>About Super Wacky PC Builder</h1>
+            <p>A smarter way to plan your next gaming rig, built by students for CP4485</p>
+
+            <h2>Why We Built This</h2>
+            <p>We're big into gaming and building PCs, so we decided that are topic for our Emerging Trends projects, we wanted to make something we'd actually use. Figuring out if your parts are compatible or if a CPU will bottleneck your GPU usually means digging through Reddit threads and articles. We're building this app to put all that info in one clean interface.</p>
+
+            <h3>What It Does (Or Will Do Soon!)</h3>
+            <h4>Part Compatibility</h4>
+            <p>Making sure your motherboard actually fits your case and supports your processor so you don't waste money.</p>
+            <h5>Keeping Track of Costs</h5>\
+            <p>PC parts get expensive fast. We're adding a way to track your total budget as you add components to your build.</p>
+            <h6>AI Checkups</h6>
+            <p>Later on we plan on attempting to hook up an AI API to look at your build and warn you if your parts are mismatched.</p>
+            <h7>Saving Builds</h7>
+            <p>Once we hook up the database, you'll be able to log in, save your dream builds, and come back to them later.</p>
+        </div>
+    );
+}
