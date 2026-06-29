b6ed25d44ec3c5e40b37b9c12571e316f28a191f
Lucas Fillier <lucas@fillier,ca>
2026-06-28 23:29:58 -0230

    Added a POST method to route.js.

--- pcBuilder/app/api/parts/route.js
+++ pcBuilder/app/api/parts/route.js
@@ -16,3 +16,25 @@ export async function GET() {
         return NextResponse.json({ error: "Failed to fetch hardware data" }, { status: 500 });
     }
 }
+
+export async function POST(request) {
+    try {
+
+        const body = await request.json();
+
+        const client = await clientPromise;
+
+        const db = client.db("wacky_pc_db");
+
+        const result = await db.collection("parts").insertOne(body);
+
+        return NextResponse.json({
+            message: "Part successfully added!",
+            insertedId: result.insertedId
+        }, { status: 201 });
+
+    } catch (error) {
+        console.error("Failed to create part", error);
+        return NextResponse.json({ error: "Failed to add part to database" }, { status: 500 });
+    }
+}
